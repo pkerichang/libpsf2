abdc03bffa9ead8720f49cfca29cff5a65fa3032
Eric Chang <pkerichang@gmail.com>
2017-01-12 13:11:44 -0800

    combine into single file.

--- include/psf.hpp
+++ include/psf.hpp
@@ -1,57 +1,101 @@
-#ifndef _PSF
-#define _PSF
+#ifndef LIBPSF_PSF_H_
+#define LIBPSF_PSF_H_
 
 #include <vector>
 #include <string>
 #include <complex>
 #include <unordered_map>
 #include <memory>
+#include <fstream>
+#include <stdexcept>
+
+#include <boost/interprocess/file_mapping.hpp>
+#include <boost/interprocess/mapped_region.hpp>
 #include <boost/variant.hpp>
+#include <boost/asio.hpp>
+
+#ifdef DEBUG
+#include <iostream>
+#define DEBUG_MSG(str) do { std::cout << str << std::endl; } while( false )
+#else
+#define DEBUG_MSG(str) do { } while ( false )
+#endif
+
+#define WORD_SIZE sizeof(uint32_t)
+#define GET_I32(buf) ntohl(*(reinterpret_cast<const uint32_t*>(buf)))
+#define GET_I64(buf) be64toh(*(reinterpret_cast<const uint64_t*>(buf)))
 
 
-typedef boost::variant<char, int, double, std::complex<double>, std::string> PSFScalar;
-typedef std::vector<PSFScalar> PSFVector;
-typedef std::vector<std::string> StrVector;
 
-typedef std::unordered_map<std::string, PSFScalar> PropDict;
-typedef std::unordered_map<std::string, std::unique_ptr<PropDict>> NestPropDict;
-typedef std::unordered_map<std::string, std::unique_ptr<PSFVector>> VecDict;
+namespace psf {
 
-
-class PSFDataSet {
- public:
-    PSFDataSet(std::string filename);
-    PSFDataSet(std::string filename, bool invert_struct);
-    ~PSFDataSet();
-
-    PropDict* get_prop_dict() const;
-    NestPropDict* get_scalar_prop_dict() const;
+    typedef boost::variant<char, int, double, std::complex<double>, std::string> PSFScalar;
+    typedef std::vector<PSFScalar> PSFVector;
+    typedef std::vector<std::string> StrVector;
     
-    StrVector* get_sweep_names() const;
-    PSFVector* get_sweep_values() const;
-    
-    int get_num_sweeps() const;
-    int get_num_points() const;
-    bool is_swept() const;
-    bool is_struct_inverted() const;
-    
-    PropDict* get_scalar_dict() const;
-    VecDict* get_vector_dict() const;
-    
- private:
-    void open(std::string filename);
-    
-    std::unique_ptr<PropDict> m_prop_dict;
-    std::unique_ptr<NestPropDict> m_scalar_prop_dict;
-    std::unique_ptr<PropDict> m_scalar_dict;
-    std::unique_ptr<VecDict> m_vector_dict;
-    std::unique_ptr<StrVector> m_swp_vars;
-    std::unique_ptr<PSFVector> m_swp_vals;
+    typedef std::unordered_map<std::string, PSFScalar> PropDict;
+    typedef std::unordered_map<std::string, std::unique_ptr<PropDict>> NestPropDict;
+    typedef std::unordered_map<std::string, std::unique_ptr<PSFVector>> VecDict;
 
-    int m_num_sweeps;
-    int m_num_points;
-    bool m_swept;
-    bool m_invert_struct;
-};
 
+    class PSFDataSet {
+    public:
+        PSFDataSet(std::string filename);
+        PSFDataSet(std::string filename, bool invert_struct);
+        ~PSFDataSet();
+        
+        PropDict* get_prop_dict() const;
+        NestPropDict* get_scalar_prop_dict() const;
+        
+        StrVector* get_sweep_names() const;
+        PSFVector* get_sweep_values() const;
+        
+        int get_num_sweeps() const;
+        int get_num_points() const;
+        bool is_swept() const;
+        bool is_struct_inverted() const;
+        
+        PropDict* get_scalar_dict() const;
+        VecDict* get_vector_dict() const;
+        
+    private:
+        void open(std::string filename);
+        
+        std::unique_ptr<PropDict> m_prop_dict;
+        std::unique_ptr<NestPropDict> m_scalar_prop_dict;
+        std::unique_ptr<PropDict> m_scalar_dict;
+        std::unique_ptr<VecDict> m_vector_dict;
+        std::unique_ptr<StrVector> m_swp_vars;
+        std::unique_ptr<PSFVector> m_swp_vals;
+        
+        int m_num_sweeps;
+        int m_num_points;
+        bool m_swept;
+        bool m_invert_struct;
+    };
+
+    inline std::string deserialize_str(const char* start, std::size_t& num_read) {
+        uint32_t len = GET_I32(start);
+        // align to word boundary (4 bytes)
+        num_read += WORD_SIZE + len + ((WORD_SIZE - len) & 3);
+        return std::string(start + WORD_SIZE, len);
+    }
+    
+    inline uint32_t deserialize_int(const char* start, std::size_t& num_read) {
+        num_read += WORD_SIZE;
+        return GET_I32(start);
+    }
+    
+    inline uint64_t deserialize_double(const char* start, std::size_t& num_read) {
+        num_read += sizeof(uint64_t);
+        return GET_I64(start);
+    }
+
+    // Decodes a single name-value entry.
+    std::size_t deserialize_entry(const char* start, uint32_t end_marker, PropDict* pdict);
+    std::size_t deserialize_prop_section(const char* start, std::size_t start_idx, uint32_t ps_type,
+                                         uint32_t end_marker, PropDict* pdict);
+}
+
+    
 #endif
--- src/psf.cpp
+++ src/psf.cpp
@@ -1,82 +1,168 @@
-#include <fstream>
-#include <stdexcept>
-#include <boost/interprocess/file_mapping.hpp>
-#include <boost/interprocess/mapped_region.hpp>
-
 #include "psf.hpp"
-#include "bindata.hpp"
 
 namespace bip = boost::interprocess;
 
-
-PSFDataSet::PSFDataSet(std::string filename) : m_invert_struct(false) {
-    open(filename);
-}
-
-PSFDataSet::PSFDataSet(std::string filename, bool invert_struct) : m_invert_struct(invert_struct) {
-    open(filename);
-}
-
-PSFDataSet::~PSFDataSet() {}
-
-
-void PSFDataSet::open(std::string filename) {
-    m_prop_dict = std::unique_ptr<PropDict>(new PropDict());
-    m_scalar_prop_dict = std::unique_ptr<NestPropDict>(new NestPropDict());
-    m_scalar_dict = std::unique_ptr<PropDict>(new PropDict());
-    m_vector_dict = std::unique_ptr<VecDict>(new VecDict());
-    m_swp_vars = std::unique_ptr<StrVector>(new StrVector());
-    m_swp_vals = std::unique_ptr<PSFVector>(new PSFVector());
-
-    m_num_sweeps = 1;
-    m_num_points = 1;
-    m_swept = false;
-
-    bip::file_mapping mapping(filename.c_str(), bip::read_only);
-    bip::mapped_region mapped_rgn(mapping, bip::read_only);
-    const char* mmap_data = static_cast<char*>(mapped_rgn.get_address());
-    std::size_t const mmap_size = mapped_rgn.get_size();
+namespace psf {
     
-    PropSection header(21, 1);
-    header.deserialize(mmap_data + 4, 4, m_prop_dict.get());
-}
+    PSFDataSet::PSFDataSet(std::string filename) : m_invert_struct(false) {
+        open(filename);
+    }
+    
+    PSFDataSet::PSFDataSet(std::string filename, bool invert_struct) : m_invert_struct(invert_struct) {
+        open(filename);
+    }
+    
+    PSFDataSet::~PSFDataSet() {}
+    
+    void PSFDataSet::open(std::string filename) {
+        // initialize members
+        m_prop_dict = std::unique_ptr<PropDict>(new PropDict());
+        m_scalar_prop_dict = std::unique_ptr<NestPropDict>(new NestPropDict());
+        m_scalar_dict = std::unique_ptr<PropDict>(new PropDict());
+        m_vector_dict = std::unique_ptr<VecDict>(new VecDict());
+        m_swp_vars = std::unique_ptr<StrVector>(new StrVector());
+        m_swp_vals = std::unique_ptr<PSFVector>(new PSFVector());
+        
+        m_num_sweeps = 1;
+        m_num_points = 1;
+        m_swept = false;
 
-PropDict* PSFDataSet::get_prop_dict() const {
-    return m_prop_dict.get();
-}
+        // create memory map file
+        bip::file_mapping mapping(filename.c_str(), bip::read_only);
+        bip::mapped_region mapped_rgn(mapping, bip::read_only);
+        const char* mmap_data = static_cast<char*>(mapped_rgn.get_address());
+        std::size_t const mmap_size = mapped_rgn.get_size();
 
-NestPropDict* PSFDataSet::get_scalar_prop_dict() const {
-    return m_scalar_prop_dict.get();
-}
+        // read first word and throw away
+        uint32_t first_word = GET_I32(mmap_data);
+        DEBUG_MSG("First word is: " << first_word);
+        std::size_t cur_idx = WORD_SIZE;
+        
+        cur_idx += deserialize_prop_section(mmap_data, cur_idx, 21, 1, m_prop_dict.get());
+        DEBUG_MSG("Cursor location = " << cur_idx);
+    }
+    
+    /**
+     * header properties section format:
+     * first word is the header section code, second word is the last index of the header section.
+     * the last word of the header section has the value of m_end_marker.
+     * the middle is a series of (type_word, name_data, value_data) bytes.
+     * type_word is a single word that indicates the value type.
+     * name_data is always a string, and value_data can be an int, a
+     * double, or a string.
+     * an int is always one word, a double is always two words, and a
+     * string has one word encoding the length, followed by the
+     * word-aligned (i.e. multiple of 4) char array data.
+     */
+    std::size_t deserialize_prop_section(const char* start, std::size_t start_idx, uint32_t ps_type,
+                                         uint32_t end_marker, PropDict* pdict) {
+        uint32_t type_id = GET_I32(start + start_idx);
+        
+        DEBUG_MSG("Prop section ID = " << type_id);
+        
+        // check that ID is valid    
+        if (type_id != ps_type) {
+            char msg[100];
+            snprintf(msg, 100, "Type ID mismatch; expect %d, got %d", ps_type, type_id);
+            throw std::runtime_error(msg);
+        }
+        
+        std::size_t cur_idx = start_idx + WORD_SIZE;
+        std::size_t end_idx = GET_I32(start + cur_idx);
+        while(cur_idx < end_idx) {
+            cur_idx += deserialize_entry(start + cur_idx, end_marker, pdict);
+            DEBUG_MSG("Cursor location = " << cur_idx);
+        }
+        
+        return cur_idx - start_idx;
+    }
 
-StrVector* PSFDataSet::get_sweep_names() const {
-    return m_swp_vars.get();
-}
+    // Decodes a single name-value entry.
+    std::size_t deserialize_entry(const char* start, uint32_t end_marker, PropDict* pdict) {
+        uint32_t type_id = GET_I32(start);
+        std::size_t loc_idx = WORD_SIZE;
 
-PSFVector* PSFDataSet::get_sweep_values() const {
-    return m_swp_vals.get();
-}
+        DEBUG_MSG("Entry ID = " << type_id);
 
-int PSFDataSet::get_num_sweeps() const {
-    return m_num_sweeps;
-}
-
-int PSFDataSet::get_num_points() const {
-    return m_num_points;
-}
-
-bool PSFDataSet::is_swept() const {
-    return m_swept;
-}
-
-bool PSFDataSet::is_struct_inverted() const {
-    return m_invert_struct;
-}
-
-PropDict* PSFDataSet::get_scalar_dict() const {
-    return m_scalar_dict.get();
-}
-
-VecDict* PSFDataSet::get_vector_dict() const {
-    return m_vector_dict.get();
+        // read name or end marker
+        std::string name;
+        PSFScalar val;
+        switch(type_id) {
+        case 33 :
+            name = deserialize_str(start + loc_idx, loc_idx);
+            DEBUG_MSG("Entry name = " << name);
+            // string type
+            val = deserialize_str(start + loc_idx, loc_idx);
+            DEBUG_MSG("Entry val = " << val);
+            pdict->emplace(name, val);
+            return loc_idx;
+        case 34 :
+            name = deserialize_str(start + loc_idx, loc_idx);
+            DEBUG_MSG("Entry name = " << name);
+            // int type
+            val = static_cast<int>(deserialize_int(start + loc_idx, loc_idx));
+            DEBUG_MSG("Entry val = " << val);
+            pdict->emplace(name, val);
+            return loc_idx;
+        case 35 :
+            name = deserialize_str(start + loc_idx, loc_idx);
+            DEBUG_MSG("Entry name = " << name);
+            // double type
+            val = static_cast<double>(deserialize_double(start + loc_idx, loc_idx));
+            DEBUG_MSG("Entry val = " << val);
+            pdict->emplace(name, val);
+            return loc_idx;
+        default :
+            if (type_id == end_marker) {
+                // end marker, nothing to read.
+                DEBUG_MSG("Entry is end marker");
+                return loc_idx;
+            }
+            // error
+            char msg[100];
+            snprintf(msg, 100, "Unrecognized Type ID: %d", type_id);
+            throw std::runtime_error(msg);
+        }
+        
+    }
+    
+    PropDict* PSFDataSet::get_prop_dict() const {
+        return m_prop_dict.get();
+    }
+    
+    NestPropDict* PSFDataSet::get_scalar_prop_dict() const {
+        return m_scalar_prop_dict.get();
+    }
+    
+    StrVector* PSFDataSet::get_sweep_names() const {
+        return m_swp_vars.get();
+    }
+    
+    PSFVector* PSFDataSet::get_sweep_values() const {
+        return m_swp_vals.get();
+    }
+    
+    int PSFDataSet::get_num_sweeps() const {
+        return m_num_sweeps;
+    }
+    
+    int PSFDataSet::get_num_points() const {
+        return m_num_points;
+    }
+    
+    bool PSFDataSet::is_swept() const {
+        return m_swept;
+    }
+    
+    bool PSFDataSet::is_struct_inverted() const {
+        return m_invert_struct;
+    }
+    
+    PropDict* PSFDataSet::get_scalar_dict() const {
+        return m_scalar_dict.get();
+    }
+    
+    VecDict* PSFDataSet::get_vector_dict() const {
+        return m_vector_dict.get();
+    }
 }
